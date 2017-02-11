7ad3a1c4ac4277d97ffdb7779eba9b2aec98ee67
EssamEid614 <essameid614@gmail.com>
2017-02-11 02:45:55 +0200

    working MDR register reading correctly next step , make code more friendly

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include "tm4c123gh6pm.h"
 
 void EnableI2CModule0(void);
+void PLL_Init(void);
 
 int main()
 {
+	PLL_Init();
 	EnableI2CModule0();
+}
+void PLL_Init(void){
+  // 0) Use RCC2
+  SYSCTL_RCC2_R |=  0x80000000;  // USERCC2
+  // 1) bypass PLL while initializing
+  SYSCTL_RCC2_R |=  0x00000800;  // BYPASS2, PLL bypass
+  // 2) select the crystal value and oscillator source
+  SYSCTL_RCC_R = (SYSCTL_RCC_R &~0x000007C0)   // clear XTAL field, bits 10-6
+                 + 0x00000540;   // 10101, configure for 16 MHz crystal
+  SYSCTL_RCC2_R &= ~0x00000070;  // configure for main oscillator source
+  // 3) activate PLL by clearing PWRDN
+  SYSCTL_RCC2_R &= ~0x00002000;
+  // 4) set the desired system divider
+  SYSCTL_RCC2_R |= 0x40000000;   // use 400 MHz PLL
+  SYSCTL_RCC2_R = (SYSCTL_RCC2_R&~ 0x1FC00000)  // clear system clock divider
+                  + (4<<22);      // configure for 80 MHz clock
+  // 5) wait for the PLL to lock by polling PLLLRIS
+  while((SYSCTL_RIS_R&0x00000040)==0){};  // wait for PLLRIS bit
+  // 6) enable use of PLL by clearing BYPASS
+  SYSCTL_RCC2_R &= ~0x00000800;
 }
 
 void EnableI2CModule0(void)
@@ -16,16 +38,19 @@ void EnableI2CModule0(void)
 	Delay = SYSCTL_RCGCGPIO_R; //delay allow clock to stabilize 
 	GPIO_PORTB_AFSEL_R|= 0x0000000C; //enable alternate functions for PB2 and PB3
 	GPIO_PORTB_ODR_R |= 0x00000008; //set PB3 (I2C SDA)  for open drain
+	GPIO_PORTB_DEN_R |= 0xFF;
 	//might need to add digital enable here or before setting open drain 
 	GPIO_PORTB_PCTL_R |=0x03;
+	I2C0_PP_R |= 0x01;
+	I2C0_MTPR_R |= 0x00000027; //set SCL clock
 	I2C0_MCR_R |= 0x00000010; //intialize mcr rigester with that value given in datasheet
-	I2C0_MTPR_R |= 0x00000009; //set SCL clock
 	//end intialization code after this is sending code
 	I2C0_MSA_R = 0x000000A6; //set slave address
 	I2C0_MDR_R = 0x00000000; //place data to send mdr register
 	I2C0_MCS_R = 0x00000007; //stop start run
-	while((I2C0_MCS_R &= 0x00000001)==1); //poll busy bit 
-	
-	
-	
+	while((I2C0_MCS_R &= 0x00000040)==1); //poll busy bit 
+	I2C0_MSA_R = 0x000000A7;
+	I2C0_MCS_R = 0x00000007;
+	while((I2C0_MCS_R &= 0x00000040)==1); //poll busy bit 
+	I2C0_MCS_R = 0x00000004;
 }
